78b2a0c5272e7b2ec10c7d638f5841bc804c4b3b
Heshercat <alfiiaqa@gmail.com>
2023-06-03 01:31:17 +0300

    Add row screenshots logic

--- src/test/java/RunConfig/RunConfigurations.java
+++ src/test/java/RunConfig/RunConfigurations.java
@@ -5,22 +5,29 @@ import Core.Config;
 import com.microsoft.playwright.*;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
 
+import java.io.File;
+import java.nio.file.Paths;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class RunConfigurations extends Config {
+public class RunConfigurations extends Config implements TestWatcher {
 
     static Playwright playwright;
     static Browser browser;
     public static HomePage homePage;
-
+    private boolean testFailed = false;
 
     @BeforeAll
     static void launchBrowser() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(Config.headless)));
-        //browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(Boolean.parseBoolean(Config.headless)));
 
+        File screenshotsDir = new File("screenshots");
+        if (!screenshotsDir.exists()) {
+            screenshotsDir.mkdir();
+        }
     }
 
     @AfterAll
@@ -28,20 +35,46 @@ public class RunConfigurations extends Config {
         playwright.close();
     }
 
-    // New instance for each test method.
     static BrowserContext context;
     public static Page page;
-
 
     @BeforeEach
     void createContextAndPage() {
         context = browser.newContext();
         page = context.newPage();
         homePage = new HomePage(page);
+        testFailed = false;
+    }
+
+    @Override
+    public void testFailed(ExtensionContext context, Throwable cause) {
+        String className = context.getRequiredTestClass().getSimpleName();
+        String methodName = context.getRequiredTestMethod().getName();
+        try {
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshots", className + "_" + methodName + "_failed.png")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void testSuccessful(ExtensionContext context) {
+        String className = context.getRequiredTestClass().getSimpleName();
+        String methodName = context.getRequiredTestMethod().getName();
+        try {
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshots", className + "_" + methodName + "_failed.png")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
     void closeContext() {
-        context.close();
+        if (page != null) {
+            page.close();
+        }
+        if (context != null) {
+            context.close();
+        }
     }
 }
